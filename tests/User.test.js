bec8c658ba88b19b4a03f631ec1b1697dbd5ff79
Anderson Tiban <andytiban12@gmail.com>
2023-02-22 09:25:05 -0500

    added test in User.test.js

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,46 @@
 const User = require('../src/User')
 
-// User tests here
+// User class property test
+describe("User class test properties", ()=>{
+    let user1 = new User("anderson", "tiban", 19);
+    // test username
+    test("username", ()=>{
+        expect(user1.username).toBe("anderson");
+    })
+    // test password
+    test("password", ()=>{
+        expect(user1.password).toBe("tiban");
+    })
+    // test age
+    test("age", ()=>{
+        expect(user1.age).toBe(19);
+    })
 
-// test username
 
-// test password
+})
+//User class method test
+describe("User class test methods", ()=>{
+    let user1 = new User("anderson", "tiban", 19);
+    // test login
+    test("login method", ()=>{
+        user1.login(user1.password);
+        expect(user1.loggedIn).toBe(true);
+    })
+    // test logout
+    test("logout method",()=>{
+        user1.logout()
+        expect(user1.loggedIn).toBe(false)
+    })
 
-// test age
 
-// test login
+})
 
-// test logout
+
+
+
+
+
+
+
+
+
