afa18f56bce68282a83ca29c76676213f089566b
Anderson Tiban <andytiban12@gmail.com>
2023-02-22 16:53:46 -0500

    added test

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -4,8 +4,10 @@ const User = require('../src/User')
 //typeof scooter === object
 describe('scooter object', () => {
   test('does something', () => {
+    let scooter = new Scooter("location1");
+    let user = new User("anderson", "tiban", "19");
     // edit this to be a real test!
-    expect(false).toEqual(true);
+    expect(typeof scooter).toEqual("object");
   }
 )
 })
@@ -13,13 +15,32 @@ describe('scooter object', () => {
 //Method tests
 describe('scooter methods', () => {
   // tests here!
+  let scooter = new Scooter("location1");
+  let user = new User("anderson", "tiban", "19");
 
   //rent method
+  test("rent method", ()=>{
+    scooter.rent()
+    expect(scooter.station).toBe(null);
+  })
 
   //dock method
+  test("dock method", ()=>{
+    scooter.dock("location1");
+    expect(scooter.station).toBe("location1");
+    
+  })
 
   //requestRepair method
+  test("request repair", ()=>{
+    scooter.requestRepair();
+    expect(scooter.isBroken).toBe(false);
+  })
 
   //charge method
+  test("charge method", ()=>{
+    scooter.recharge();
+    expect(scooter.charge).toBe(100);
+  })
 
 })
--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -3,13 +3,32 @@ const User = require('../src/User')
 const ScooterApp = require('../src/ScooterApp')
 
 // ScooterApp tests here
+describe("scooter app class", ()=>{
+    let scooter = new Scooter("location1");
+    let user = new User("anderson", "tiban", 19);
+    let scootApp = new ScooterApp();
+    // register user
+    test("register user method", ()=>{
+        scootApp.registerUser(user.username, user.password, user.age);
+        expect(scootApp.registeredUsers).toHaveProperty(user.username);
+    })
 
-// register user
+    // log in
+    test("login method", ()=>{
+        scootApp.loginUser(user, user.password);
+        expect(user.loggedIn).toBe(true);
+    })
 
-// log in
+    // log out
+    test("logout method", ()=>{
+        scootApp.logoutUser(user);
+        expect(user.loggedIn).toBe(false);
+    })
 
-// log out
+    // rent scooter
 
-// rent scooter
+    // dock scooter
 
-// dock scooter
+
+})
+
